732ff97485abba14afebd0c257dca6b6340dd29e
IgorU <umanets@gmail.com>
2024-08-29 16:39:57 +0200

    fix starting development server

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const { execSync } = require('child_process');
 const os = require('os');
 const path = require('path');
+const fs = require('fs');
 
 const platform = os.platform();
 const scriptPath = path.join(__dirname, 'setup-ts-project.ps1');
+const projectNameFilePath = path.join(__dirname, 'project_name.txt');
 
 try {
     // Run the setup script based on the platform
@@ -15,9 +17,11 @@ try {
         execSync('bash setup-ts-project.sh', { stdio: 'inherit' });
     }
 
-    // After setting up the project, start the development server
-    console.log('Starting the development server with nodemon and ts-node...');
-    execSync('npx nodemon --exec ts-node ./src/index.ts', { stdio: 'inherit' });
+    const projectName = fs.readFileSync(projectNameFilePath, 'utf8').trim();
+    console.log(`Project directory created: ${projectName}`);
+    console.log(`Navigating to ${projectName} and starting the development server with nodemon and ts-node...`);
+    execSync(`cd ${projectName} && npx nodemon --exec ts-node ./src/index.ts`, { stdio: 'inherit' });
+
 
 } catch (error) {
     console.error('Error occurred during setup or execution:', error);
--- setup-ts-project.sh
+++ setup-ts-project.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 read -p "Enter the name of the project directory: " projectName
 mkdir -p "$projectName/src"
 cd "$projectName"
@@ -8,3 +9,5 @@ npx tsc --init
 echo '{ "compilerOptions": { "target": "ES6", "module": "commonjs", "rootDir": "./src", "outDir": "./dist", "esModuleInterop": true, "strict": true }, "include": ["src/**/*"], "exclude": ["node_modules"] }' > tsconfig.json
 echo "console.log('Hello, TypeScript with Node.js!');" > src/index.ts
 echo "Project $projectName has been set up successfully."
+cd ..
+echo $projectName > "project_name.txt"
